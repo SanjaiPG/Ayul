d002ab407b3a2dd80408b34cea8075bef2da9913
SanjaiPG <sanjaigukesan8686@gmail.com>
2025-08-29 23:05:47 +0530

    Bottom Navigation Bar design

---
+++ ayul/lib/navigationBar.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBody: true,
+      body: Center(
+        child: Text(
+          "Page $_selectedIndex",
+          style: const TextStyle(fontSize: 24),
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Container(
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(40, 0, 0, 0).withOpacity(0.3),
+            borderRadius: BorderRadius.circular(30),
+          ),
+          child: BottomNavigationBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            selectedItemColor: Colors.white,
+            unselectedItemColor: const Color.fromARGB(255, 212, 212, 212),
+            type: BottomNavigationBarType.fixed,
+            currentIndex: _selectedIndex,
+            onTap: (index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.auto_graph),
+                label: "Graph",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                label: "Search",
+              ),
+              BottomNavigationBarItem(icon: Icon(Icons.book), label: "Book"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

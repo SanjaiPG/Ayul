e886712d6dd5ee5b83578e3f67e790ed0d561378
Sanjjiiev S <sanjjiiev005@gmail.com>
2025-10-06 19:10:01 +0530

    firebase integrated

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:sizer/sizer.dart';
@@ -7,6 +9,10 @@ import '../widgets/custom_error_widget.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   bool _hasShownError = false;
 
